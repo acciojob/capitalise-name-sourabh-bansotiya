6147aeca3f7ffc487aef30aa111583a974e6c248
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-03 14:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     inputField.addEventListener('blur', () => {
         const inputValue = inputField.value;
         const upperCaseValue = inputValue.toUpperCase();
-        
+
+      inputField.value = upperCaseValue;
+		
         alert("Enter your name: " + upperCaseValue);
     });
 });
